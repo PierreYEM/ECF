c860c9f3f79aad3821271cefb458be91f2d42c7a
PierreYEM <118944652+PierreYEM@users.noreply.github.com>
2022-12-16 16:52:33 +0100

    maj

--- ecf.js
+++ ecf.js
@@ -7,17 +7,10 @@ const randomMeal = "https://www.themealdb.com/api/json/v1/1/random.php",
 
 
 
-let change = document.querySelector('#change'),
-    x = "";
-// ingredients = document.querySelector('.ingredients'),
+let change = document.querySelector('#change');
 
 
-
-
-
-
-
-///Rangement de variable dans des tableaux
+///déclaration de tableaux facilitant mon code///
 let picture = [...document.querySelectorAll('.picMeal')],
 
     ///Variables noms de plat///
@@ -28,16 +21,13 @@ let picture = [...document.querySelectorAll('.picMeal')],
     modal = [...document.querySelectorAll('.modal')],
 
     ingredients = [...document.querySelectorAll('.ingredients')],
-    gauche = [...document.querySelectorAll('.gauche')];
+    gauche = [...document.querySelectorAll('.gauche')],
 
-///Rassemblement des différents tableaux en 1 avec objets   
+    footerLogo = [...document.querySelectorAll('.t')];
+
+
+///Rassemblement des différents tableaux en 1 avec objets ( pour le Fun)  
 let info = [{ picture, nameMeal }, { list, modal }];
-
-
-
-// Instructions
-///Ajout des 3 plats aléatoires via la fonction établie
-
 
 
 ///Bouton changer = consiste à rafraichir la page afin que le fetch se relance
@@ -67,75 +57,27 @@ for (let i = 0; i < list.length; i++) {
     })
 
 };
+
+///appel de la fonction principale
 getRandomMeal();
+// for (let i = 0; i < footerLogo.length; i++) {
+//     footerLogo.addEventListener('click', () => {
 
-async function getRecipe() {
-    fetch(randomMeal)
-        .then(data => data.json())
-        .then(json => {
-
-            //Déclaration tableau des ingrédients
-            let tab = [json.meals[0].strIngredient1,
-            json.meals[0].strIngredient2,
-            json.meals[0].strIngredient3,
-            json.meals[0].strIngredient4,
-            json.meals[0].strIngredient5,
-            json.meals[0].strIngredient6,
-            json.meals[0].strIngredient7,
-            json.meals[0].strIngredient8,
-            json.meals[0].strIngredient9,
-            json.meals[0].strIngredient10,
-            json.meals[0].strIngredient11,
-            json.meals[0].strIngredient12,
-            json.meals[0].strIngredient13,
-            json.meals[0].strIngredient14,
-            json.meals[0].strIngredient15,
-            json.meals[0].strIngredient16,
-            json.meals[0].strIngredient17,
-            json.meals[0].strIngredient18,
-            json.meals[0].strIngredient19,
-            json.meals[0].strIngredient20,];
-
-            for (let c = 0; c < 20; c++) {
-
-                if (!(tab[c] == null) ^ (tab[c] == "")) {
-
-                    ingredients[i].innerHTML = ingredients[i].innerHTML + '<div>' + tab[c] + '</div>';
-                    let listIng = [...document.querySelectorAll("div")];
-                    console.log(listIng);
-                }
-            }
-        })
-
-    let a = document.createElement('div'),
-        li = document.querySelector('li'),
-        ul = [...document.querySelectorAll('ul')];
-    console.log(ul);
-    await fetch("https://www.themealdb.com/api/json/v1/1/list.php?i=list")
-        .then(data => data.json())
-        .then(data => {
-            console.log(data);
+//         alert("En construction par manque de temps \nl'administration vous présente ses plus plates excuses pour le dérangement");
+//     })
+// }
 
 
 
-
-
-
-
-
-
-        })
-
-
-}
 
 async function getRandomMeal() {
 
+    ///Ajout des 3 plats aléatoires via la fonction établie
     for (let i = 0; i < picture.length; i++) {
         ingredients[i].innerHTML = "";
         let response = await fetch(randomMeal);
         response = await response.json();
-
+        //images correspondants aux plats
         picture.forEach(elment => {
             info[0].picture[i].innerHTML = '<img src=' + response.meals[0].strMealThumb + ' alt="' + response.meals[0].strMeal + '">';
             info[0].nameMeal[i].innerHTML = response.meals[0].strMeal
@@ -165,28 +107,48 @@ async function getRandomMeal() {
         response.meals[0].strIngredient19,
         response.meals[0].strIngredient20,];
 
-
+        ///liste des ingrédients///
         for (let c = 0; c < 20; c++) {
 
-            ///ajout de nom des ingrédients///
+            // si l'emplacement de l'ingrédient est différent de " " ou exclusif Null alors je procède aux instructions de cration de div et de l'ajout de l'ingrédient du plat//
             if (!(tab[c] == null) ^ (tab[c] == "")) {
 
-                console.log(tab[c]);
-
                 let nameIng = document.createElement('div'),
-
                     detailIng = document.createElement('div');
+
+                // instructions de création de bloc aliments
                 textIng = document.createTextNode(tab[c]);
                 nameIng.classList.add('nameIng');
                 detailIng.classList.add('detailIng');
                 detailIng.setAttribute('data-id', tab[c]);
-
                 nameIng.appendChild(textIng);
                 nameIng.appendChild(detailIng);
 
-
-
+                //ajout des ingredients
                 ingredients[i].appendChild(nameIng);
+
+                // description de chaque aliments ( en construction)
+
+                // let a = [...document.querySelectorAll('[data-id]')];
+                // b = a.map(element => '' + element.dataset.id + '');
+
+                // fetch(allIng)
+                //     .then(data => data.json())
+                //     .then(ing => {
+
+                //         for (let i = 0; i < ing.meals.length; i++) {
+
+                //             if (ing.meals[i].strIngredient == a[c].dataset.id) {
+
+                //                 a[c].innerHTML = ing.meals[i].strDescription;
+                //                 if
+                //                     (ing.meals[i].strDescription == null ^ ing.meals[i].strDescription == "") { a[c].innerHTML = "No description available" }
+                //             } else { console.log("nope"); }
+
+
+                //         }
+                //     })
+                // description de chaque aliments ( en construction)
 
             }
 
@@ -194,62 +156,5 @@ async function getRandomMeal() {
 
     }
 
-    let a = [...document.querySelectorAll('[data-id]')];
-    b = a.map(element => '' + element.dataset.id + '');
-    console.log(a);
-    console.log(b);
-    console.log(a[0]);
-    console.log(a[0].dataset.id);
-    for (let c = 0; c < a.length; c++) {
-
-
-
-        fetch(allIng)
-            .then(data => data.json())
-            .then(ing => {
-                console.log(ing);
-                console.log(a[0].dataset.id);
-                for (let i = 0; i < ing.meals.length; i++) {
-
-
-                    if (ing.meals[i].strIngredient === "Oil") {
-
-                        a[c].innerHTML = ing.meals[i].strDescription;
-                        if
-                            (ing.meals[i].strDescription == null ^ ing.meals[i].strDescription == "") { a[c].innerHTML = "No description available" }
-                    } else { console.log("nope"); }
-                }
-
-
-
-            })
-
-
-
-    }
 }
 
-// let a = document.querySelector('.ab')
-
-// fetch("https://www.themealdb.com/api/json/v1/1/list.php?i=list")
-//     .then(data => data.json())
-//     .then(json => {
-//         console.log(json);
-//         console.log(json.meals[0].strIngredient);
-//         console.log(json.meals.length);
-//         for (let i = 0; i < json.meals.length; i++) {
-//             if (json.meals[i].strIngredient == "Salmon") {
-
-//                 console.log(json.meals[i].strDescription);
-
-//                 a.innerHTML = json.meals[i].strDescription;
-//                 if
-//                     (json.meals[i].strDescription == null) { a.innerHTML = "No description available" }
-//             }
-
-//         }
-
-
-
-
-//     })
